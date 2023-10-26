fb01b1b8e51f060a0d2f54109e5f1cdcbd9c9807
jorgen-lund <jorgen.h.lund@gmail.com>
2023-10-26 13:13:37 +0200

    Refactored code

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,17 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Counter {
-    uint count; //Cannot be negative (uint is only positive)
-
-    // Code inside the constructor will be called once during deployment of
-    // the contract
-    constructor() {
-        count = 0;
-    }
-    // A view function, doesnt cost gas
-    function getCount() public view  returns(uint){
-        return count;
-    }
+    uint public count = 0; 
 
     // A write function, have to pay gas (POST, PATCH/PUT)
     function incrementCount() public {
